eb05dbfead0e33a8739cd6ebc7fe0f678885586b
Matt <bryantm1122@gmail.com>
2023-05-05 15:38:51 +0200

    Initial Commit

---
+++ AmeriPal/AmeriPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmeriPalApp.swift
+//  AmeriPal
+//
+//  Created by Matt Bryant on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AmeriPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
